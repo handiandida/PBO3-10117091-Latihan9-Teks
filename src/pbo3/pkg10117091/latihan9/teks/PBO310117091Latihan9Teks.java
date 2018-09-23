a92895a5e117ff46752bc89122c55961ffc2370b
Dida Handian <handiandida@gmail.com>
2018-09-23 19:50:11 +0700

    Teks

---
+++ src/pbo3/pkg10117091/latihan9/teks/PBO310117091Latihan9Teks.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117091.latihan9.teks;
+
+/* @author
+* NAMA  : DIDA HANDIAN
+* KELAS : PBO3
+* NIM   : 10117091
+* Deskripsi Program : Program ini berisi contoh penggunaan type data char
+*/
+public class PBO310117091Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       char c;
+       char tab = '\t';
+       char newline = '\n';
+       c = 'A';
+        System.out.println("Hasi" + newline + "Char:" + tab + "berisi " + c);
+    }
+    
+}
